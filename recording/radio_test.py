eb45b83455be72053c80769d728d6502ae60a1c9
Simon Kamronn <simon@kamronn.dk>
2016-01-17 16:53:50 +0100

    Dockerfile

--- recording/radio_test.py
+++ recording/radio_test.py
@@ -7,7 +7,7 @@ import contextlib
 import numpy as np
 
 SAMPLE_RATE = 11025
-url = 'http://live-icy.gss.dr.dk/A/A03L.mp3'
+url = 'http://live-icy.gss.dr.dk/A/A04L.mp3'
 recording_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
 with contextlib.closing(wave.open('radio_test.wav', 'w')) as of:
@@ -21,8 +21,8 @@ with contextlib.closing(wave.open('radio_test.wav', 'w')) as of:
 
 time.sleep(60)
 
-url = 'http://localhost:5000/match/'
-# url = 'http://192.168.99.100:8000/match/'
+# url = 'http://localhost:5000/match/'
+url = 'http://192.168.99.100:8000/match/'
 # url = 'http://46.101.177.208:8000/match/'
 files = {'audio_file': open('radio_test.wav', 'rb')}
 response = requests.post(url, files=files, data={'recording_time': recording_time,
